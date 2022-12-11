2fe92065a4b2917e1150868670b01b14f62a3175
sm18aeh <94913381+sm18aeh@users.noreply.github.com>
2022-12-11 18:34:17 +0000

    V1.0
    Added a function for creating bar charts.
    Added saving graphs to picture in each graph function.
    Improved comments.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def read_file(file_name):
     -df having empty data removed and unnecessary colums removed
     -countries_df being a transposed version of df
     """
-    #converted file from xls to xlsx in order to avoid
+    #converted WB file from xls to xlsx in order to avoid
     #xlrd module conflict
     df = pd.read_excel(file_name,
                        sheet_name="Data",header=3)
@@ -36,7 +36,7 @@ def line_graph(countries, countries_df,labels):
     Takes the dataframe the to plot the data with;
     Takes a list of two strings that contain the label names
     
-    Produces a line plot
+    Produces a line plot of inputted countries
     """
     #setting values for the x-axis markings
     start_year = int(min(countries_df.index))
@@ -53,8 +53,44 @@ def line_graph(countries, countries_df,labels):
     plt.ylabel(labels[1])
     plt.grid()
     plt.legend()
+    #saving and displaying line graph
+    name = "line_graph_" + labels[0] + "_" + labels[1] + ".png"
+    plt.savefig(name,bbox_inches="tight")
     plt.show()
 
+def bar_chart(countries, countries_df,years_range,labels):
+    """
+    Takes a list of countries to be plotted;
+    Takes the dataframe the to plot the data with;
+    Takes a list containing two integers for the year range;
+    Takes a list of two strings that contain the label names
+    
+    Produces bar chart of inputted countries
+    """
+    #list of decades in the given year range
+    decades = [i for i in range(years_range[0],years_range[1]+1,10)]
+    str_decades = [str(i) for i in decades]
+    #number of countries
+    n_cnt = len(countries)
+    plt.figure(figsize=[24,15])
+    bar_width = 1
+    #list of units in bar widths used to adjust bar positioning
+    #to center each year group to their axis
+    width_range = [i for i in range(-n_cnt//2,(n_cnt//2)+1,bar_width)]
+    for i in range(n_cnt):
+        plt.bar([item+width_range[i] for item in decades],
+                countries_df[countries[i]].loc[str_decades],
+                label=countries[i],width=bar_width)
+    #adjusting labels
+    plt.xticks(decades)
+    plt.legend(prop={'size': 15})
+    plt.xlabel(labels[0])
+    plt.ylabel(labels[1])
+    #saving and displaying bar chart
+    name = "bar_chart_" + labels[0] + "_" + labels[1] + ".png"
+    plt.savefig(name,bbox_inches="tight")
+    plt.show()
+    pass
 
 def heatmap(country, dataframes, labels):
     """
@@ -92,27 +128,30 @@ def heatmap(country, dataframes, labels):
     ax.set_xticklabels(labels,rotation=35,ha="right",
                        rotation_mode="anchor")
     ax.set_yticks(range(len(labels)), labels=labels)
+    #displaying the values to each square of the heatmap
     for i in range(len(labels)):
         for j in range(len(labels)):
             ax.text(j, i, round(heat_df.to_numpy()[i, j], 2),
                     ha="center", va="center", color="black")
+    #saving and displaying heat map
+    name = country + "_heat_map.png"
+    plt.savefig(name,bbox_inches="tight")
     plt.show()
 
 
 if __name__ == "__main__":
     #obtaining CO2/capita emissions data
     co2_df, co2_val_df = read_file("WB CO2 Emissions mT Per Capita.xlsx")
-    # print(countries_df.head(5))
     
     #Selecting the countries to create the line graph with
     countries = ["United Kingdom","Germany","Romania","Bulgaria",
                  "Ghana","Nigeria"]
-    line_graph(countries,co2_df,["Year","CO2 Emissions (Metric Tons)/Capita"])
+    line_graph(countries,co2_df,["Year","CO2 Emissions (Metric Tons) Per Capita"])
     
-    #Selecting the countr
+    #obtaining access to electricity data
     countries_df,values_df = read_file("WB Access to Electricity.xlsx")
     line_graph(["Ghana","Indonesia"],countries_df,
-               ["Year","Access to Electricty (%)"])
+               ["Year","Access to Electricity (%)"])
     #obtaining GDP/capita data
     gdp_df, gdp_val_df = read_file("WB GDP Per Capita.xlsx")
     
@@ -122,13 +161,24 @@ if __name__ == "__main__":
     #obtaining Population Growth (%) data
     pop_growth_df, pop_growth_val_df = read_file("WB Population Growth.xlsx")
     
+    #creating heatmaps for the Japan and the UK
     heatmap("Japan",[co2_df,gdp_df,electric_df,pop_growth_df],
-            ["CO2 (Metric Tons)/Capita","GDP/Capita","Electricity Usage (KWh)/Capita",
-             "Population Growth (%)"])
+            ["CO2 (Metric Tons)/Capita","GDP/Capita",
+             "Electricity Usage (KWh) Per Capita","Population Growth (%)"])
     heatmap("United Kingdom",[co2_df,gdp_df,electric_df,pop_growth_df],
-            ["CO2 (Metric Tons)/Capita","GDP/Capita","Electricity Usage (KWh)/Capita",
-             "Population Growth (%)"])
-
+            ["CO2 (Metric Tons)/Capita","GDP/Capita",
+             "Electricity Usage (KWh) Per Capita","Population Growth (%)"])
+    
+    #obtaining the under 5 mortality rate data
+    mortality_df,mortality_val_df = read_file("WB Mortality Under 5.xlsx")
+    #selecting the countries to create the bar charts with
+    countries = ["United Kingdom","Germany","United States",
+                 "Bulgaria","Romania","Thailand","Ghana",
+                 "Nigeria","Pakistan"]
+    #creating bar charts for the selected countries
+    bar_chart(countries, gdp_df,[2000,2020],["Year","GDP Per Capita"])
+    bar_chart(countries,mortality_df,[2000,2020],
+              ["Year","Mortality Rate Under 5 (per 1000)"])
     
          
     
